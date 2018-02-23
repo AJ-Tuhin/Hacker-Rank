1e500789060ba2cb5451122281fbab33b4b3e13f
Md. Al Jobaed <tuhincse001@gmail.com>
2018-02-23 15:40:15 +0600

    Add files via upload

---
+++ Algorithms/Warmup/009. Birthday Cake Candles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Cake_Candles
+{
+    class Program
+    {
+        static int birthdayCakeCandles(int n, int[] ar)
+        {
+            int a = 0;
+            int h = ar[0];
+            foreach (int i in ar)
+            {
+                if (i >= h)
+                {
+                    h = i;
+                }
+            }
+            foreach (int i in ar)
+            {
+                if (h == i)
+                {
+                    a++;
+                }
+            }
+            return a;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = birthdayCakeCandles(n, ar);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
