6fcbd4694a38663f7d0c321f2f06ff2d6ff3f266
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-14 22:55:20 +0600

    Add files via upload

---
+++ Algorithms/Warmup/CamelCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static int camelcase(string s)
+        {
+            int a = 1;
+            char[] s1 = s.ToCharArray();
+
+            foreach (char t in s1)
+            {
+                if (t <= 'Z' && t >= 'A')
+                {
+                    a++;
+                }
+            }
+            return a;
+        }
+
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            int result = camelcase(s);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
