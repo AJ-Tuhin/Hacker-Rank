c54d195f9afbd656b0d81bd850bfc24b62adaee6
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-16 15:38:37 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day3IntroToConditionalStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3IntroToConditionalStatements
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 !=0)  
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (n % 2 == 0 && ((n >= 2 && n <= 5) || n > 20))
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+            else if (n % 2 ==0 && n >= 6 && n <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
