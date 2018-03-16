8a5d7496fc06f733bcb8d19a5d75ee398ac08803
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-16 16:20:14 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day5Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Loops
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", n, i, i*n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
