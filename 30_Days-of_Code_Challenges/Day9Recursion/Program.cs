94fe608c2fa1d1217f4d892abf3db95746f1d3f5
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-22 00:18:17 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day9Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Recursion
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            int factorial = 1;
+            for (int i = n; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = factorial(n);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
