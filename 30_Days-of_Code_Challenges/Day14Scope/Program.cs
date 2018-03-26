fd1eb3f3e309a06cc860350a39e7b7c4fe282503
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-27 00:55:26 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day14Scope/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+            Console.ReadKey();
+        }
+    }
+
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] e)  
+        {
+            this.elements = e;
+        }
+        public int computeDifference()
+        {
+            int b = elements.Length - 1;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int a;
+                for (int j = b; j >= 0 ; j--)
+                {
+                    a = elements[i] - elements[j];
+                    a = Math.Abs(a);
+                    if (maximumDifference <= a)
+                    {
+                        maximumDifference = a;
+                    }
+                } 
+            }
+            return maximumDifference;
+        }
+    }
+}
