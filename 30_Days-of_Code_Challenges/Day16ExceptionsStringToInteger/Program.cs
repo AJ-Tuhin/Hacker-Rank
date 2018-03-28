ceb287b7a81e0b0cc1e380081c88b941a00d6158
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-28 19:53:51 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day16ExceptionsStringToInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16ExceptionsStringToInteger
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(Convert.ToInt32(S));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
