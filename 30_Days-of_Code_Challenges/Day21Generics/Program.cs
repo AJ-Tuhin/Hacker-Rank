2b04073e241f69c0f0abae21f443351fb3eac200
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-31 11:58:35 +0600

    Add files via upload

---
+++ 30_Days-of_Code_Challenges/Day21Generics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray<T>(T[] intArray)
+        {
+            foreach(T item in intArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
